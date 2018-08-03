c128d92479ff45aa36bda61ca15d6d10c887b821
BlackAnge <37332133+BlackAnge@users.noreply.github.com>
2018-08-03 17:15:19 +0300

    smtp2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'work-list85646.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
@@ -101,5 +102,4 @@ Rails.application.configure do
       authentication:       :plain,
       enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { :host => 'work-list85646.herokuapp.com' }
 end
