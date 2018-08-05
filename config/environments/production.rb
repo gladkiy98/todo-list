019e5855d926f7ffb16cb345cfa54459291927af
BlackAnge <37332133+BlackAnge@users.noreply.github.com>
2018-08-05 23:41:45 +0300

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            'p98tretyak@gmail.com',
-      password:             'Petya98Tretyak',
+      user_name:            'ENV['SENDGRID_USERNAME']',
+      password:             'ENV['SENDGRID_USERNAME']',
       authentication:       :plain,
       enable_starttls_auto: true
   }
