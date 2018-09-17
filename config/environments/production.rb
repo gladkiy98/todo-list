200e6ffacc016be72e881b588dc18ccdc0471000
BlackAnge <p98tretyak@gmail.com>
2018-09-17 14:20:49 +0300

    change mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,13 +91,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'work-list85646.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'my-todo-app123124234' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'p98tretyak@gmail.com',
+    user_name:            'mistercater1@gmail.com',
     password:             'Petya98Tretyak',
     authentication:       :plain,
     enable_starttls_auto: true
